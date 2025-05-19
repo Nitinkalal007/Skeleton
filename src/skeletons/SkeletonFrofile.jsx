4eb7666d3a8452569cf846c0be63d0bdfdec6f60
Nitinkalal007 <nitinkalal01@gmail.com>
2025-05-19 13:09:04 +0530

    Change

--- src/skeletons/SkeletonFrofile.jsx
+++ src/skeletons/SkeletonFrofile.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import SkeletonElement from './skeletonElement'
+import SkeletonElement from './SkeletonElement'
 
 const SkeletonFrofile = ({theme}) => {
     const themeclass=theme||'light';
